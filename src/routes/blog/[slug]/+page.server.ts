cc1b6ddb236495af57bbc2df07afe80e933e94b0
Abhijeet Sandil <abhijeetsandil@gmail.com>
2024-01-04 22:15:17 +0530

    load data

---
+++ src/routes/blog/[slug]/+page.server.ts
@@ -0,0 +1,7 @@
+export function load() {
+    return {
+        title: "This is title",
+        content: "Dummy page",
+        html: "<h1>Heading</h1>"
+    }
+}
--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
 	import { page } from "$app/stores";
+	export let data;
 </script>
 
 {$page.params.slug}
+{data.title} // No dollar sign for regular props
+{@html data.html} //Dont do this with untrusted data XSS attack
