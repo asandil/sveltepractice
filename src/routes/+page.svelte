1f61f3226dd6b52b20affd3afa8e868c63a2d2d6
Abhijeet Sandil <abhijeetsandil@gmail.com>
2024-01-04 21:46:52 +0530

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	let count = 0;
+	$: doubled = count * 2;
+
+	function increment() {
+		count += 1;
+	}
+
+	$: if (count >= 10) {
+		alert("count is dangerously high!");
+		count = 0;
+	}
+</script>
+
+{doubled}
+<button on:click={increment}>
+	Clicked {count}
+	{count === 1 ? "time" : "times"}
+</button>
